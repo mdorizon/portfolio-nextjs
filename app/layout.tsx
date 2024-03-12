3df0da2191119d90dcd14957a991a78a9e792822
mdorizon <mdorizon17@gmail.com>
2024-03-12 11:52:55 +0100

    :tada: example nextjs project imported as base code

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,28 @@
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
+import { SpeedInsights } from "@vercel/speed-insights/next"
+ 
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'NextJS Dashboard',
+  },
+  description: 'Wakusay NextJS Training DashBoard with login.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
+
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>
+        {children}
+        <SpeedInsights />
+      </body>
     </html>
-  )
+  );
 }
