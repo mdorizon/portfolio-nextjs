7b23ffee747d10ba33170c4fa51a46ab58f4a6d9
mdorizon <mdorizon17@gmail.com>
2024-04-08 14:14:46 +0200

    :lipstick: footer section -> new style

--- src/components/sections/footer.tsx
+++ src/components/sections/footer.tsx
@@ -8,23 +8,23 @@ export default function Footer() {
 
   return (
     <>
-      <footer className="flex flex-dir-col">
-        <div className="contact-me flex flex-dir-col align-center" id="contact">
-          <div className="contact-container flex flex-dir-col align-center">
+      <footer>
+        <div className="contact-me" id="contact">
+          <div className="contact-container">
             <h3>Me contacter</h3>
             <div className="contact">
               <h1 className="text-copy">mdorizon17@gmail.com</h1>
             </div>
           </div>
         </div>
-        <div className="footer-links flex flex-dir-col align-center">
-          <div className="footer-container flex justify-between">
-            <div className="infos flex flex-dir-col align-start justify-center">
+        <div className="footer-links">
+          <div className="footer-container">
+            <div className="infos">
               <p>MAIL : <span className="text-copy">mdorizon17@gmail.com</span></p>
               <p>TEL : <a className="tel-hover" href="tel:+33768501007">+33 7 68 50 10 07</a></p>
               <SocialsContainer hasScrollDown={false} />
             </div>
-            <div className="nav-items flex flex-dir-col">
+            <div className="nav-items">
               {navLinks.map((link) => (
                 <a key={link.id} href={link.href}>
                   <li>{link.text}</li>
